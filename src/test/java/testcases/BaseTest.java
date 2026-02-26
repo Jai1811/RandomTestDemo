87e51473c37fa32cba3a0a9421647250a586edfd
Jai Vyas <vyasjai98@gmail.com>
2026-02-26 02:06:39 +0000

    boilerplate

---
+++ src/test/java/testcases/BaseTest.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import testutils.DriverManager;
+
+public class BaseTest {
+    private final WebDriver driver;
+
+    public BaseTest() {
+        this.driver = DriverManager.getDriver();
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        DriverManager.quitDriver();
+    }
+
+}
---
+++ src/test/java/testutils/DriverManager.java
@@ -0,0 +1,41 @@
+package testutils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DriverManager {
+    private static WebDriver driver;
+
+    private DriverManager() {}
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("profile.default_content_setting_values.notifications", 2);
+            prefs.put("profile.default_content_setting_values.geolocation", 2);
+            prefs.put("profile.default_content_setting_values.media_stream_mic", 2);
+
+            options.addArguments("--start-maximized");
+            options.setExperimentalOption("prefs", prefs);
+            options.addArguments("--disable-notifications");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
